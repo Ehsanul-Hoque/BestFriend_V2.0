3c2f7ec19d088535abe6f09e63a98a7b65cb8db5
Ehsanul-Hoque <ehsanfahad01@gmail.com>
2017-07-16 12:12:33 +0600

    Update HomeActivity.java

--- app/src/main/java/com/sprinjinc/bestfriend/HomeActivity.java
+++ app/src/main/java/com/sprinjinc/bestfriend/HomeActivity.java
@@ -2,10 +2,13 @@ package com.sprinjinc.bestfriend;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
